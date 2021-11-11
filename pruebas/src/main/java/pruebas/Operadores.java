1a7039d9e1310c0a71f2a665dbbe3ceb9a86cc60
Juancho2406 <jdsaavedra@ucundinamarca.edu.co>
2021-11-11 17:57:26 -0500

    Inicio
    	Clase 1: Repositorios
    	clase 2: Tipos de datos

---
+++ pruebas/src/main/java/pruebas/Operadores.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		//ctrl+eliminar linea
+		
+		//matematicos
+		/*
+		+
+		-
+		*
+		/
+		
+		*/
+		
+		int edad1=10;
+		int edad2=10;
+		
+		//int res=edad1+edad2;
+		
+		
+		//aplica a tipos de datos String
+		//String nombre="Carlos";
+		String apellido="Lopez";
+		//String res=nombre+apellido;
+		//System.out.println("edad1+edad2="+res);
+		
+		double alto=175.5;
+		
+		float ancho= 205.7F;
+		
+		float resDiv=(float)alto/ancho;//Quedarme con el float de ese double
+		
+		byte valor =127;// > -128 a + 127
+		
+		boolean esMartes= true;
+		boolean horario19=false;
+		System.out.println("el valor de verdad es "+esMartes+"&&"+horario19+"=");
+		
+		int edadAdulto =18;
+		boolean tieneCarnet=true;
+		String nombre="Jose";
+		float altura=0.75F;
+		char letra='M';
+		
+		boolean puedoConducir = (edadAdulto>= 18 )&& tieneCarnet && (nombre=="jose")|| (altura>1.0) && (letra!='G');
+		
+		boolean aux=true;
+		boolean baux=true;
+		
+		
+		boolean resAux=aux^baux;
+		
+		
+		byte[] datos=new byte[] {0,1,0,1,0};
+		
+		System.out.println("resAux="+resAux);
+		
+		
+		boolean a=false;
+		boolean b=false;
+		boolean c= a && b;
+		System.out.println(c? 1:0);
+		
+		int r=(c ? 1:0);// Operador ternario 
+		
+		Scanner teclado= new Scanner (System.in);
+		//int a=teclado.nextInt();
+		//int b=teclado.nextInt();
+		
+		
+		//int suma= a+b;
+		
+		//System.out.println("La suma es="+suma);
+		
+		teclado.close();// Se cierra para que no quede abierto un espacio de memoria
+		
+		// Con el click derecho se crea un toogle breakpoint
+		//cambiamos la vista, y a medida que oprimimos F6 se ejecuta linea a linea, F8 para que se ejecuten todas las lineas hasta terminar 
+		// Este proceso se llama debug
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
